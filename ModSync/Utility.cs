896150fa1b76ad2b8099f15fff52bfedabb5f5be
corter <5890013+c-orter@users.noreply.github.com>
2024-06-03 21:03:00 -0500

    Initial, working commit of modsync

---
+++ ModSync/Utility.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.IO;
+using Aki.Common.Utils;
+
+namespace ModSync
+{
+    public static class Utility
+    {
+        public static List<string> GetFilesInDir(string dir)
+        {
+            var files = new List<string>();
+
+            foreach (var directory in VFS.GetDirectories(dir))
+            {
+                if (VFS.Exists(Path.Combine(directory, ".do-no-update")))
+                    continue;
+
+                foreach (var file in GetFilesInDir(directory))
+                    files.Add(file);
+            }
+
+            foreach (var file in VFS.GetFiles(dir))
+                files.Add(file);
+
+            return files;
+        }
+
+        public static string GetTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            if (File.Exists(tempDirectory))
+                return GetTemporaryDirectory();
+            else
+            {
+                Directory.CreateDirectory(tempDirectory);
+                return tempDirectory;
+            }
+        }
+
+        public static void CopyFilesRecursively(string source, string target) => CopyFilesRecursively(new DirectoryInfo(source), new DirectoryInfo(target));
+
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            foreach (FileInfo file in source.GetFiles())
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+        }
+    }
+}
