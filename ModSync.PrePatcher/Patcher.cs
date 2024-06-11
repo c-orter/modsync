7ed9cb9453a48fa9e009f0f395d26a1863ec74f0
Carter Grimmeisen <cpg0009@uah.edu>
2024-06-10 23:40:50 -0500

    Move update process into BepInEx preload patcher. (I know, I know. But if its stupid and it works...)

---
+++ ModSync.PrePatcher/Patcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+
+namespace ModSync.PrePatcher
+{
+    public static class Patcher
+    {
+        // List of assemblies to patch
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+        public static void CopyFilesRecursively(string source, string target) => CopyFilesRecursively(new DirectoryInfo(source), new DirectoryInfo(target));
+
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            foreach (FileInfo file in source.GetFiles())
+                file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+        }
+
+        // Patches the assemblies
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            // Patcher code here
+            var pendingUpdatePath = Path.Combine(Directory.GetCurrentDirectory(), ".pending-update");
+
+            if (File.Exists(pendingUpdatePath))
+            {
+                var updateDir = File.ReadAllText(pendingUpdatePath).Trim();
+
+                var clientModsDir = Path.Combine(updateDir, "clientMods");
+                if (Directory.Exists(clientModsDir))
+                    CopyFilesRecursively(clientModsDir, Directory.GetCurrentDirectory());
+
+                var serverModsDir = Path.Combine(updateDir, "serverMods");
+                if (Directory.Exists(serverModsDir))
+                    CopyFilesRecursively(serverModsDir, Directory.GetCurrentDirectory());
+
+                Directory.Delete(updateDir, true);
+                File.Delete(pendingUpdatePath);
+            }
+        }
+    }
+}
--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,24 +1,24 @@
 import { $ } from "bun";
-import AdmZip from "adm-zip"
 
 const packageJson = await Bun.file("package.json").json();
 
-await $`rm -rf dist`.nothrow().quiet()
-await $`rm corter-modsync-*-*.zip`.nothrow().quiet()
+await $`rm -rf dist`.nothrow().quiet();
 
 await $`mkdir -p dist`;
 await $`mkdir -p dist/user/mods/corter-modsync/src`;
 await $`mkdir -p dist/BepInEx/plugins`;
+await $`mkdir -p dist/BepInEx/patchers`;
 await $`cp package.json dist/user/mods/corter-modsync/`;
 await $`cp src/* dist/user/mods/corter-modsync/src`;
 
-await $`dotnet build --configuration Release`.cwd("ModSync/")
-await $`cp ModSync/bin/Release/Corter-ModSync.dll dist/BepInEx/plugins/`
+await $`dotnet build --configuration Release`.cwd("ModSync/");
+await $`cp ModSync/bin/Release/Corter-ModSync.dll dist/BepInEx/plugins/`;
+await $`cp ModSync.PrePatcher/bin/Release/Corter-ModSync-Patcher.dll dist/BepInEx/patchers/`;
 
-const serverZip = new AdmZip();
-serverZip.addLocalFolder("dist/user/", "user");
-await serverZip.writeZipPromise(`corter-modsync-server-${packageJson.version}.zip`)
+await $`7z a -tzip corter-modsync-server-${packageJson.version}.zip user/`
+	.cwd("dist/")
+	.quiet();
 
-const clientZip = new AdmZip();
-clientZip.addLocalFolder("dist/BepInEx/", "BepInEx");
-await clientZip.writeZipPromise(`corter-modsync-client-${packageJson.version}.zip`)
+await $`7z a -tzip corter-modsync-client-${packageJson.version}.zip BepInEx/`
+	.cwd("dist/")
+	.quiet();
