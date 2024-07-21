397fe75accb8c994a9ee3c35eef1cffaa686ca7f
corter <5890013+c-orter@users.noreply.github.com>
2024-07-20 20:52:34 -0400

    chore(deps): use pnpm instead of bun for the sake of compatbility with Rider

---
+++ ModSync.Server/scripts/build.cjs
@@ -0,0 +1,26 @@
+﻿const { config, rm, mkdir, cp, pushd, popd, exec } = require("shelljs");
+const packageJson = require("../package.json");
+
+rm("-rf", "../dist");
+mkdir("-p", "../dist/user/mods/corter-modsync/src");
+mkdir("-p", "../dist/BepInEx/plugins");
+mkdir("-p", "../dist/BepInEx/patchers");
+cp("package.json", "../dist/user/mods/corter-modsync/");
+cp("src/*", "../dist/user/mods/corter-modsync/src");
+
+pushd("-q", "../");
+exec("dotnet build --configuration Release");
+popd("-q");
+
+cp("../ModSync/bin/Release/Corter-ModSync.dll", "../dist/BepInEx/plugins/");
+cp(
+	"../ModSync.PrePatcher/bin/Release/Corter-ModSync-Patcher.dll",
+	"../dist/BepInEx/patchers/",
+);
+
+pushd("-q", "../dist");
+config.silent = true;
+exec(`7z a -tzip Corter-ModSync-Server-v${packageJson.version}.zip user/`);
+exec(`7z a -tzip Corter-ModSync-Client-v${packageJson.version}.zip BepInEx/`);
+config.silent = false;
+popd("-q");
---
+++ ModSync.Server/scripts/bump-version.cjs
@@ -0,0 +1,60 @@
+﻿const inc = require("semver/functions/inc");
+const valid = require("semver/functions/valid");
+const path = require("node:path")
+const { pushd, popd, sed } = require("shelljs");
+
+const packageJson = require("../package.json");
+const currentVersion = packageJson.version;
+
+let level = "patch";
+let newVersion = null;
+
+if (process.argv.length > 2) {
+	if (process.argv[2].startsWith("--")) {
+		level = process.argv[2].slice(2);
+	} else if (valid(currentVersion)) {
+		newVersion = process.argv[2];
+	}
+}
+
+newVersion = newVersion ?? inc(currentVersion, level);
+
+sed(
+	"-i",
+	`"version": "${currentVersion}"`,
+	`"version": "${newVersion}"`,
+	"package.json",
+);
+
+pushd("-q", "../ModSync");
+sed(
+	"-i",
+	`"${currentVersion}"`,
+	`"${newVersion}"`,
+	"Properties/AssemblyInfo.cs",
+);
+sed(
+	"-i",
+	`"${currentVersion}"`,
+	`"${newVersion}"`,
+	"Plugin.cs",
+);
+popd("-q");
+
+pushd("-q", "../ModSync.PrePatcher");
+sed(
+	"-i",
+	`"${currentVersion}"`,
+	`"${newVersion}"`,
+	"Properties/AssemblyInfo.cs",
+);
+popd("-q")
+
+pushd("-q", "../ModSync.Tests");
+sed(
+	"-i",
+	`"${currentVersion}"`,
+	`"${newVersion}"`,
+	"Properties/AssemblyInfo.cs",
+);
+popd("-q")
