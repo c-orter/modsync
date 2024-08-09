b2952599f6daa7f35fb64546ccaf42027caae5e7
corter <5890013+c-orter@users.noreply.github.com>
2024-08-08 21:57:49 -0400

    chore: bump version to 0.8.0

--- ModSync/Properties/AssemblyInfo.cs
+++ ModSync/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("6337fafc-25ba-4062-b61c-12a614297ae5")]
 
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyVersion("0.8.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
