3868f1945b50c91226e6942593b132b8ca42a52f
corter <5890013+c-orter@users.noreply.github.com>
2024-08-19 11:30:11 -0400

    chore: finish bumping version :facepalm:

--- ModSync.Tests/Properties/AssemblyInfo.cs
+++ ModSync.Tests/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("abcad858-d5d2-4331-800f-4aee9b339b52")]
 
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
+[assembly: AssemblyVersion("0.8.1")]
+[assembly: AssemblyFileVersion("0.8.1")]
--- ModSync/Plugin.cs
+++ ModSync/Plugin.cs
@@ -20,7 +20,7 @@ namespace ModSync;
 using SyncPathFileList = Dictionary<string, List<string>>;
 using SyncPathModFiles = Dictionary<string, Dictionary<string, ModFile>>;
 
-[BepInPlugin("corter.modsync", "Corter ModSync", "0.8.0")]
+[BepInPlugin("corter.modsync", "Corter ModSync", "0.8.1")]
 public class Plugin : BaseUnityPlugin
 {
     private static readonly string MODSYNC_DIR = Path.Combine(Directory.GetCurrentDirectory(), "ModSync_Data");
